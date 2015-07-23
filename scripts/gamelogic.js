a8de63c3b8ab234a789801e6519fc74222c0220c
Nicole2106 <nicole621@gmx.de>
2015-07-23 11:36:05 +0200

    test

--- scripts/gamelogic.js
+++ scripts/gamelogic.js
@@ -11,39 +11,39 @@ $(function () {
                 .css('background-color', '#00FF00')
                 .prop('disabled', true);
 
-        var not_clicked_new = [];
-        var j = 0;
-        var l = not_clicked.length;
-        for (var i = 0; i < not_clicked.length; i++) {
-            var id = parseInt($(this).attr('id').substring(3));
-            if (i !== not_clicked.indexOf(id)) {
-                not_clicked_new[j] = not_clicked[i];
-                j++;
-            }
-        }
-        not_clicked = not_clicked_new;
+        var id = parseInt($(this).attr('id').substring(3));
+
+        not_clicked = delete_field(not_clicked, not_clicked.indexOf(id));
         win_or_game_end();
         ki_play();
         win_or_game_end();
     });
+    $('#new_game').click(function(){$(document).ready()});
 });
 
 function ki_play() {
     var random_number = parseInt(Math.random() * (not_clicked.length + 1));
+    while(random_number===not_clicked.length){
+        random_number = parseInt(Math.random() * (not_clicked.length + 1));
+    }
     var field = not_clicked[random_number];
     $('#btn' + field)
             .css('background-color', '#FF0000')
             .prop('disabled', true);
-    var not_clicked_new = [];
-    var j = 0;
-    var l = not_clicked.length;
+
+    not_clicked = delete_field(not_clicked, random_number);
+}
+
+function delete_field(list, index){
+    var j=0;
+    var not_clicked_new=[];
     for (var i = 0; i < not_clicked.length; i++) {
-        if (i !== random_number) {
+        if (i !== index) {
             not_clicked_new[j] = not_clicked[i];
             j++;
         }
     }
-    not_clicked = not_clicked_new;
+    return not_clicked_new;
 }
 
 function win_row() {
