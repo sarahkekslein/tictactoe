dccea29adb9c12538188ea0307c18672676be8f6
Sarahkekslein <sarahbensberg@gmx.de>
2015-07-24 13:08:49 +0200

    neu

--- index.php
+++ index.php
@@ -10,20 +10,20 @@ $tpl = new Smarty();
 $tpl->assign('isLoggedIn', isset($_SESSION['user']));
 
 //list, which contains the allowed sites
-define('WHITELIST', array('login', 'logout', 'edit', 'game', 'login_post', 'registration', 'registration_post', 'chat', 'ranking', 'chat_post'));
+$whitelist = array('login', 'logout', 'edit', 'game', 'login_post', 'registration', 'registration_post', 'chat', 'ranking', 'chat_post');
 //list, which contains the sites a user can see although he is not logged in
-
-define('NOT_LOGGED_IN', array('login', 'registration', 'registration_post', 'login_post', 'ranking'));
+$not_logged_in = array('login', 'registration', 'registration_post', 'login_post', 'ranking');
 
 //when logged in and visiting index-page redirect to game
 if (isset($_SESSION['user']) && empty($_GET['page'])) {
     $_GET['page'] = 'game';
 }
 
-if (isset($_GET['page']) && in_array($_GET['page'], WHITELIST) && ( isset($_SESSION['user']) || in_array($_GET['page'], NOT_LOGGED_IN))) {
+if (isset($_GET['page']) && in_array($_GET['page'], $whitelist) && ( isset($_SESSION['user']) || in_array($_GET['page'], $not_logged_in))) {
     require $_GET['page'] . '.php';
 }
 
 $tpl->display('templates/main.tpl');
+
 ?>
 
