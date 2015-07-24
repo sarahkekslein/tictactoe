0042948906f5760567c8ab55423a5e4916c6a0b8
deiki <klein.deike@gmx.de>
2015-07-24 09:50:03 +0200

    text

--- registration.php
+++ registration.php
@@ -9,7 +9,7 @@ if (isset($_GET['fail'])) {
         $tpl->assign('message', 'Probier es noch einmal!');
     }
     if ($fehler === '2') {
-        $tpl->assign('message', 'Das Passwort ist zu kurz!');
+        $tpl->assign('message', 'Das Passwort muss mindestens 8 Zeichen haben.');
     }
     if ($fehler === '3') {
         $tpl->assign('message', 'Die Passwörter stimmen nicht überein!');
