3afdb83b4dbf580cf2e7d33faa078bd076b2103c
Sarahkekslein <sarahbensberg@gmx.de>
2015-07-24 10:45:38 +0200

    neu

--- templates_c/a5cab98200155968e18c0ce38c84ba5470a61eb8.file.main.tpl.php
+++ templates_c/a5cab98200155968e18c0ce38c84ba5470a61eb8.file.main.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.20, created on 2015-07-23 14:30:03
+<?php /* Smarty version Smarty-3.1.20, created on 2015-07-24 10:41:38
          compiled from "templates\main.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:2326455adf9afcb7838-56446303%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'a5cab98200155968e18c0ce38c84ba5470a61eb8' => 
     array (
       0 => 'templates\\main.tpl',
-      1 => 1437654597,
+      1 => 1437727123,
       2 => 'file',
     ),
   ),
@@ -43,6 +43,9 @@ $_valid = $_smarty_tpl->decodeProperties(array (
             </form>
             <form method="post" action="index.php?page=chat">
                 <input id="chat" type="submit" value="Chat" class="menu">
+            </form>
+            <form method="post" action="index.php?page=ranking">
+                <input id="ranking" type="submit" value="Ranking" class="menu">
             </form>
             <form method="post" action="index.php?page=logout">
                 <input id="logout" type="submit" value="Logout" class="menu">
--- templates_c/b1f2f2cc9f62e3646c56af32f7b126d2f50d1a43.file.index.tpl.php
+++ templates_c/b1f2f2cc9f62e3646c56af32f7b126d2f50d1a43.file.index.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.20, created on 2015-07-22 15:33:38
+<?php /* Smarty version Smarty-3.1.20, created on 2015-07-24 10:41:38
          compiled from ".\templates\index.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:841955adf90a0ca898-52502729%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'b1f2f2cc9f62e3646c56af32f7b126d2f50d1a43' => 
     array (
       0 => '.\\templates\\index.tpl',
-      1 => 1437571602,
+      1 => 1437727123,
       2 => 'file',
     ),
   ),
@@ -19,12 +19,27 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'unifunc' => 'content_55adf90a107795_36805159',
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_55adf90a107795_36805159')) {function content_55adf90a107795_36805159($_smarty_tpl) {?><div id="button">
-    <form id="register" method="post" action="index.php?page=registration">
-    <input type="submit" value="Registrieren">
-    </form>
-    <form id="login" method="post" action="index.php?page=login">
-    <input type="submit" value="Anmelden">
-    </form>    
+<?php if ($_valid && !is_callable('content_55adf90a107795_36805159')) {function content_55adf90a107795_36805159($_smarty_tpl) {?><link rel="stylesheet" href="./styles/index.css">
+<div class="center">
+    <h1 class="red">
+        Tic
+    </h1>
+    <h1>
+        Tac
+    </h1>
+    <h1 class="blue">
+        Toe
+    </h1>
+    <div id="button">
+        <form id="register" method="post" action="index.php?page=registration">
+            <input type="submit" value="Registrieren" class="button">
+        </form>
+        <form id="login" method="post" action="index.php?page=login">
+            <input type="submit" value="Anmelden" class="button">
+        </form>
+        <form id="ranking" method="post" action="index.php?page=ranking">
+            <input type="submit" value="Ranking" class="button">
+        </form>    
+    </div>
 </div>
 <?php }} ?>
