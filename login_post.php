d3122ac38d02aeee26ba1b976bde7a4227b192fd
deiki <klein.deike@gmx.de>
2015-07-24 10:24:29 +0200

    fehlermeldung

--- login_post.php
+++ login_post.php
@@ -9,7 +9,9 @@ if (!isset($_GET['page'])) {
     $email = $_POST['name'];
 
 
-
+    if(empty($_POST['pw']) || empty($_POST['name'])){
+        header('Location:index.php?page=login&fail=1');
+    }
 
     $statement = $connection->prepare("SELECT password FROM user WHERE name = :username OR email =:email");
     $statement->bindValue(':username', $username);
@@ -24,13 +26,17 @@ if (!isset($_GET['page'])) {
     $statement->execute();
     $id = $statement->fetch();
     $statement->closeCursor();
+    
+    echo($hash);
 
     if ($hash !== false) {
         if (password_verify($password, $hash['password'])) {
             $_SESSION['user'] = $id['id'];
             header('Location:index.php?page=game');
+        }else{
+            header('Location:index.php?page=login&fail=2');
         }
     }
-    header('Location:index.php?page=login&fail=1');
+    header('Location:index.php?page=login&fail=3');
 }
 ?>
